246f2d1664e7ffbd8983e5e7d41db2f00e4a9229
Alex Martishin <asmartishin@gmail.com>
2023-12-17 23:54:06 +0100

    Initial commit

---
+++ src/eyes.ts
@@ -0,0 +1,29 @@
+export function setupEyes() {
+  const anchor = document.getElementById("gopher-image")!
+  const eyes = document.querySelectorAll(".eye")
+
+  const rect = anchor.getBoundingClientRect()
+
+  const anchorX = rect.left + rect.width / 2
+  const anchorY = rect.top + rect.height / 2
+
+  document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const angleDeg = getAngleDeg(mouseX, mouseY, anchorX, anchorY)
+    console.log(angleDeg)
+
+    eyes.forEach((eye) => {
+      ;(eye as HTMLElement).style.transform = `rotate(${180 + angleDeg}deg)`
+    })
+  })
+}
+
+function getAngleDeg(mouseX: number, mouseY: number, anchorX: number, anchorY: number) {
+  const diffX = mouseX - anchorX
+  const diffY = mouseY - anchorY
+
+  const rad = Math.atan2(diffY, diffX)
+  return (rad * 180) / Math.PI
+}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import "./style.css"
+
+import eyeImage from "./assets/eye.png"
+import gopherImage from "./assets/gopher.png"
+import { setupEyes } from "./eyes.ts"
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <img src='${gopherImage}' id="gopher-image" alt="Gopher Image" />
+    <div id="eyes">
+      <img class="eye" src="${eyeImage}" style="bottom: 68px; left: 52px" alt="Gopher Left Eye">
+      <img class="eye" src="${eyeImage}" style="bottom: 70px; left: 103px" alt="Gopher Right Eye">
+    </div>
+    <p class="read-the-docs">
+      <a href="https://github.com/tty-monkey/gopher-eye-tracking">Source code</a>
+    </p>
+  </div>
+`
+
+setupEyes()
